63bf065e609de72c4239bcd319fb0df797dfda9d
unknown <adel.rjili@isimg.tn>
2026-03-19 11:57:04 +0100

    feat: add vercel Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Gallery from './components/Gallery';
 import Services from './components/Services';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <Services />
       <Contact />
       <Footer />
+      <Analytics />
     </div>
   );
 }
